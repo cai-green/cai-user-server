eb0aa6af3fe74a4967d882cb8538fd7d133a11db
csq <1368141872@qq.com>
2023-01-07 19:00:46 +0800

    Initial commit

---
+++ src/main/java/com/cai/CaiFramworkApplication.java
@@ -0,0 +1,13 @@
+package com.cai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaiFramworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaiFramworkApplication.class, args);
+    }
+
+}
